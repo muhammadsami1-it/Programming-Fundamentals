76816b0b5a60fcc3318dae719db07c57f530f24a
muhammadsami1-it <muhammadsami1567@gmail.com>
2025-10-23 19:36:34 +0500

    .

---
+++ labs/lab5/C practical lab week 5 Q1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+int age;
+printf("Enter yor age");
+scanf("%d",&age);
+if(age>=18)
+{ printf("You are eligible for voting");
+}
+else
+printf("You are not eligible");
+}
