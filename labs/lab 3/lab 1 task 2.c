86799e98af1b1860958078bb5291e250e7dba13f
muhammadsami1-it <muhammadsami1567@gmail.com>
2025-10-25 14:21:07 +0500

    .

---
+++ labs/lab 3/lab 1 task 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    printf("This program checks if a number is even or odd.\n");
+
+    printf("Enter a number: ");
+    scanf("%d", &a);
+
+    (a % 2 == 0) ? printf("Even\n") : printf("Odd\n");
+
+    return 0;
+}
