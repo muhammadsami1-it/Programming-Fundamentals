f04dae0955b98495783beea8e0121c6de10a610c
muhammadsami1-it <muhammadsami1567@gmail.com>
2025-11-15 21:35:57 +0500

    Leet code q 4

---
+++ leetcode/q 4.c
@@ -0,0 +1,9 @@
+bool checkPerfectNumber(int num) {
+    if (num <= 1) return false;
+    int sum = 1;
+    for (int i = 2; i*2 <= num; i++)
+        if (num%i==0)
+            sum+=i;
+    if(sum==num) return true;
+    else return false;
+}
